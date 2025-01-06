0095c34e5c90b23a90162946903b106858c8b3c1
Muhammad Azam <mazam1573@gmail.com>
2025-01-07 01:54:07 +0500

    Contact us page and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     // { path: '', redirectTo: '/', pathMatch: 'full' },
-    { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'contact-us', component: ContactComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '', component: HomeComponent },
 ];
